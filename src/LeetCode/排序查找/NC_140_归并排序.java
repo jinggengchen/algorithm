8b10f0087520061d0aaf92862d9ce874aecab25b
jinggengchen <mutong0410@163.com>
2022-04-07 00:09:46 +0800

    归并排序

---
+++ src/LeetCode/排序查找/NC_140_归并排序.java
@@ -0,0 +1,34 @@
+package LeetCode.排序查找;
+
+public class NC_140_归并排序 {
+    // 归并排序
+    public void mergeSort(int[] nums, int l, int r) {
+        if (l >= r) return;
+        int mid = (l + r) / 2;
+        mergeSort(nums, l, mid);
+        mergeSort(nums, mid + 1, r);
+        merge(nums, l, mid, r);
+    }
+
+    // 归并排序的merge方法
+    private void merge(int[] nums, int l, int mid, int r) {
+        int[] tmp = new int[r - l + 1];
+        int i = l, j = mid + 1, k = 0;
+        while (i <= mid && j <= r) {
+            if (nums[i] <= nums[j]) {
+                tmp[k++] = nums[i++];
+            } else {
+                tmp[k++] = nums[j++];
+            }
+        }
+        while (i <= mid) {
+            tmp[k++] = nums[i++];
+        }
+        while (j <= r) {
+            tmp[k++] = nums[j++];
+        }
+        for (int m = 0; m < tmp.length; m++) {
+            nums[l + m] = tmp[m];
+        }
+    }
+}
