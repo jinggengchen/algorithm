5b67505f60f4a5ed8a2a339ab0493ad0a5244c4c
jinggengchen <jinggengchen@baidu.com>
2022-11-04 15:25:00 +0800

    无重复字符的最小长度

--- src/LeetCode/滑动窗口/LC_3_无重复字符的最长子串.java
+++ src/LeetCode/滑动窗口/LC_3_无重复字符的最长子串.java
@@ -6,7 +6,31 @@ import java.util.Map;
 public class LC_3_无重复字符的最长子串 {
 
     public static void main(String[] args) {
-        System.out.println(lengthOfLongestSubstring("pwwkew"));
+        System.out.println(lengthOfLongestSubstring2("pwwkew"));
+    }
+
+
+
+    public static int lengthOfLongestSubstring2(String s) {
+        // 当前字符出现的次数
+        Map<Character, Integer> windons = new HashMap<>();
+        int left = 0, right = 0;
+        int res = 0;
+        char[] chars = s.toCharArray();
+        while(right < s.length()) {
+            char a = chars[right];
+            windons.put(a, windons.getOrDefault(a, 0) + 1);
+            right ++;
+            // 当前字符出现了多次，就需要缩小窗口范围
+            while (windons.get(a) > 1) {
+                char b = chars[left];
+                windons.put(b, windons.getOrDefault(b,0) - 1);
+                left ++;
+            }
+            res = Math.max(res, right - left);
+        }
+        return res;
+
     }
     public static int lengthOfLongestSubstring(String s) {
         char[] c = s.toCharArray();
@@ -18,6 +42,34 @@ public class LC_3_无重复字符的最长子串 {
             }
             res = Math.max(res, j - i + 1);
             map.put(c[j], j + 1);
+
+        }
+        return res;
+    }
+
+    public static int lengthOfLongestSubstring1(String s) {
+
+
+        Map<Character, Integer> windows = new HashMap<>();
+
+        int left = 0, right = 0;
+        int res = 0;
+        char[] chars = s.toCharArray();
+        while (right < chars.length) {
+            char c = chars[right];
+            right ++;
+            windows.put(c, windows.getOrDefault(c, 0) + 1);
+            System.out.println("window: [" + left + "," + right + ")");
+            // 判断左侧窗口是否要收缩
+            while (windows.get(c) > 1) {
+                // d 是将移出窗口的字符
+                char d = chars[left];
+                // 缩小窗口
+                left++;
+                windows.put(d, windows.getOrDefault(d, 0) - 1);
+                // 进行窗口内数据的一系列更新
+            }
+            res = Math.max(res, right - left);
 
         }
         return res;
--- src/LeetCode/滑动窗口/LC_76_最小覆盖子串.java
+++ src/LeetCode/滑动窗口/LC_76_最小覆盖子串.java
@@ -9,7 +9,60 @@ import java.util.Map;
  */
 public class LC_76_最小覆盖子串 {
     public static void main(String[] args) {
-        System.out.println(minWindow ("bbaa", "aba"));
+        System.out.println(minWindow1 ("PPADOBECODEBANC", "ABC"));
+    }
+
+    // P P A D O B E C O D E B A N C
+    // A B C
+
+    // need: {A:1,B:1,C:1}
+    // P P A D O B E C
+    // {A:1,B:1,C:1}
+
+
+    public static String minWindow1(String s, String t) {
+        int left = 0, right = 0;
+        Map<Character, Integer> windows = new HashMap<>();
+        Map<Character, Integer> need = new HashMap<>();
+        char[] ss = s.toCharArray();
+        char[] tt = t.toCharArray();
+        int len = Integer.MAX_VALUE;
+        int count = 0;
+        int start = 0;
+        int end = 0;
+        for (Character chars: tt) {
+            need.put(chars, need.getOrDefault(chars, 0) + 1);
+        }
+        while (right < ss.length) {
+            char s1 = ss[right];
+            right ++;
+            // TODO
+            if (need.containsKey(s1)) {
+                windows.put(s1, windows.getOrDefault(s1, 0) + 1);
+                if (windows.get(s1).equals(need.get(s1))) {
+                    count ++;
+                }
+            }
+
+            //
+            while (count == need.size()) {
+                if (right - left < len) {
+                    start = left;
+                    end = right;
+                    len = right - left;
+                }
+                char s2 = ss[left];
+                left ++;
+                if (need.containsKey(s2)) {
+                    if (need.get(s2).equals(windows.get(s2))) {
+                        count --;
+                    }
+                    windows.put(s2, windows.getOrDefault(s2, 0) - 1);
+                }
+
+            }
+        }
+        return len == Integer.MAX_VALUE ? "" : s.substring(start, end);
     }
     public static String minWindow(String s, String t) {
         Map<Character, Integer> need = new HashMap<>();
